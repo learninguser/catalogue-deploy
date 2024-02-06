dbbb7b02abc1f26a0786a6656aec312ad6618878
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-02-06 02:11:11 +0100

    Pass values from terraform to ansible playbook

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,8 @@
 
 component=$1
 environment=$2 #dont use env here, it is reserved in linux
+app_version=$3
 
 yum install python3.11-devel python3.11-pip -y
 pip3.11 install ansible botocore boto3
-ansible-pull -U https://github.com/learninguser/roboshop-ansible-roles-tf -e component=$component -e env=$environment main-tf.yaml
+ansible-pull -U https://github.com/learninguser/roboshop-ansible-roles-tf -e component=$component -e env=$environment -e app_version=$app_version main-tf.yaml
